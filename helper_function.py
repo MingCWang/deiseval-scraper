db09b9b2600d77acc374a822e4e37d340fd8fe4f
Ming-Shih <minstone@gmail.com>
2023-10-13 18:05:08 -0400

    complete scraper

--- helper_function.py
+++ helper_function.py
@@ -6,6 +6,15 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
 
 
+def driver():
+    '''creates driver instance'''
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(options=chrome_options) # use this line if you want to run the scraper in the background
+    # driver = webdriver.Chrome()
+    driver.get("https://registrar-prod.unet.brandeis.edu/registrar/schedule/search")
+    return driver
+
 def select_subject(index, option, driver):
     select_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "subject")))
     select = Select(select_element)
@@ -25,5 +34,51 @@ def select_term(i, count, driver):
         count += 1
     return i
    
+def get_data_from_popup(driver, links):
+    '''get course description from the popup'''
+    popup_link = links[0].get_attribute('href')[18:].split(',')[0].split("%")[0]
+    popup_link = "https://registrar-prod.unet.brandeis.edu/registrar/schedule/" + popup_link
+    driver.get(popup_link)
+    description = driver.find_element(By.XPATH, "//div[@id='coursepage']/p").text.split('\n')[0]
+    return description
+
+def find_class_table(driver):
+    '''find the table that contains the course data'''
+    class_table = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'classes-list')))
+    class_list = class_table.find_elements(By.TAG_NAME, 'tr')
+    return class_list
+
+def get_course_data(class_list, i, driver):
+    '''get course data from the table'''
+    data = class_list[i].find_elements(By.TAG_NAME, 'td')
+    links = data[0].find_elements(By.TAG_NAME, 'a')
+    syllabus = ''
+    if len(links) == 2:
+        syllabus = links[1].get_attribute('href')
+    else:
+        syllabus = 'Not Provided'
+    course = links[0].text
+    courseTitle = data[1].find_element(By.TAG_NAME, 'strong').text
+    span = data[1].find_elements(By.TAG_NAME, 'span') # get the requirements from the list of span tags
+    requirements = [requirement.text for requirement in span]
+    instructor = data[4].text
+    description = get_data_from_popup(driver, links)
+    
+    return course, courseTitle, syllabus, instructor, requirements, description
+
+def navigate_to_current_term(driver, value, count):
+    if count == 3: # loop through terms based on the options value
+        count = 1
+        value += 8
+    else:
+        value += 1
+        count += 1
+    selected_option = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, f'//select[@name="strm"]/option[@value="{str(value)}"]')))
+    selected_text = selected_option.text.strip() # get the text of the option
+    selected_option.click() # select the option
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "searchsubmit"))).click() # click the search button
+    
+    return selected_text, value
+
    
    
--- scraper.py
+++ scraper.py
@@ -5,51 +5,60 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
 
-from helper_function import select_subject, select_term
+from helper_function import select_subject, select_term, get_data_from_popup, find_class_table, get_course_data, navigate_to_current_term, driver
 import time
-import os
-
-
-
-def driver():
-    '''creates driver instance'''
-    driver = webdriver.Chrome()
-    driver.get("https://registrar-prod.unet.brandeis.edu/registrar/schedule/search")
-    return driver
+import json
 
 def scraper(driver, start, end):
     '''main scraper function'''
     count = 0
     value = start - 1
-    try:
-        while value < end:
-            if count == 3:
-                count = 1
-                value += 8
-            else:
-                value += 1
-                count += 1
-            select_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "strm")))
-            select = Select(select_element)
-            select.select_by_value(str(value))
-            print(value)
-            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "searchsubmit"))).click()
-            time.sleep(10)
-           
-            # select_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "subject")))
-            # select = Select(select_element)
-            # for index, option in enumerate(select.options):
-            #     select_subject(index, option, driver)
-    except Exception as e: 
-        print('Error: ', e)
+    data_list = []
+    course_count = 0
+    while value < end:
+        selected_text, value = navigate_to_current_term(driver, value, count)
+        try: 
+            while True: # loop through all the pages, until the next button is not found
+                class_list = find_class_table(driver)
+                for i in range(1, len(class_list)): # skip the first row, then store the course data in a list in json format
+                    class_list = find_class_table(driver) # find the table again to avoid stale element error
+                    course, courseTitle, syllabus, instructor, requirements, description = get_course_data(class_list, i, driver)
+                    driver.back() # go back to the previous page after scraping course description
+                    data_list.append({
+						'course': course,
+						'courseTitle': courseTitle,
+						'syllabus': syllabus,
+						'instructor': instructor,
+						'term': selected_text,
+						'requirements': requirements,
+						'description': description
+					})
+                    course_count += 1
+                    print(f'{course_count} courses scraped')
+                    WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//a[contains(text(), 'Next →')]"))).click()
+        except Exception as e:
+            driver.get("https://registrar-prod.unet.brandeis.edu/registrar/schedule/search")
+            continue
 
+    return data_list, course_count
 
 def main():
 	start = 1201 
-	end = 1233
+	end = 1201
 	webdriver = driver()
-	scraper(webdriver, start, end)
-	print(f'\n==========================================\nDOWNLOAD COMPLETED\n==========================================')
+	start_time = time.time()
+	data_list, course_count = scraper(webdriver, start, end)
+	end_time = time.time()
+	duration = round(end_time - start_time, 1)
+	with open('data.json', 'w') as file:
+		json.dump(data_list, file, indent=4)
+	print("\n========== DOWNLOAD COMPLETED ===========\n")
+	print(
+        f"""Duration: {duration} seconds
+Courses Downloaded: {course_count}
+        """
+    )
+	print("==========================================\n")
 
 
 main()
